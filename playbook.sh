db59700596232be1892d88811d7cc488cb9f30f9
teanoon <anticipationvu@gmail.com>
2021-08-19 08:51:54 +0800

    init commit

---
+++ playbook.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install ansible
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
